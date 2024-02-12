0d4c0080a9c27091882f0d1b799042a9aa1cbee3
Sofya Dymchenko sofya.dymchenko@univ-grenoble-alpes.fr <dymchens-ext@f-dahu.u-ga.fr>
2024-02-12 17:12:29 +0100

    added Allen Cahn and Convection PDEs as they are in R3 code repo

---
+++ src/pde/ac.py
@@ -0,0 +1,54 @@
+import numpy as np
+import deepxde as dde
+import scipy
+
+from . import baseclass
+
+
+class AllenCahn1D(baseclass.BaseTimePDE):
+
+    def __init__(self, datapath="ref/AC.mat", geom=[-1, 1], time=[0, 1], nu=0.0001):
+        super().__init__()
+        # output dim
+        self.output_dim = 1
+        # domain
+        self.geom = dde.geometry.Interval(*geom)
+        timedomain = dde.geometry.TimeDomain(*time)
+        self.geomtime = dde.geometry.GeometryXTime(self.geom, timedomain)
+        self.bbox = geom + time
+
+        # PDE
+        def ac_pde(x, u):
+            u_t = dde.grad.jacobian(u, x, i=0, j=1)
+            u_xx = dde.grad.hessian(u, x, i=0, j=0)
+            return u_t + 5 * u ** 3 - 5 * u - nu * u_xx
+
+        self.pde = ac_pde
+        self.set_pdeloss()
+
+        # refdata
+        self.load_ref_data(datapath)
+
+        # BCs
+        def ic_func(x):
+            xx = x[:, 0:1] # x component
+            return xx ** 2 * np.cos(np.pi * xx)
+
+        self.add_bcs([{
+            'component': 0,
+            'function': ic_func,
+            'bc': (lambda _, on_initial: on_initial),
+            'type': 'ic'
+        }, {
+            'component': 0,
+            'function': (lambda _: -1), # as in Wu et al 2023
+            'bc': (lambda _, on_boundary: on_boundary),
+            'type': 'dirichlet'
+        }])
+
+        # train settings
+        self.training_points()  # default
+
+    def load_ref_data(self.datapath):
+        data = scipy.io.loadmat(datapath)
+        self.ref_data = np.hstack([data['x'].T, data['uu']]).astype(np.float32) # X,T = 512, 1+201 and initially float64
---
+++ src/pde/convection.py
@@ -0,0 +1,95 @@
+import numpy as np
+import deepxde as dde
+import scipy
+
+from . import baseclass
+
+
+class Convection1D(baseclass.BaseTimePDE):
+
+    def __init__(self, , geom=[0, 2*np.pi], time=[0, 1], beta=30.0, nu=0.0, rho=0.0):
+        super().__init__()
+        # output dim
+        self.output_dim = 1
+        # domain
+        self.geom = dde.geometry.Interval(*geom)
+        timedomain = dde.geometry.TimeDomain(*time)
+        self.geomtime = dde.geometry.GeometryXTime(self.geom, timedomain)
+        self.bbox = geom + time
+
+        self.beta = beta
+
+        # PDE
+        def convection_pde(x, u):
+            u_x = dde.grad.jacobian(u, x, i=0, j=0)
+            u_t = dde.grad.jacobian(u, x, i=0, j=1)
+            return u_t + beta * u_x
+            
+        def reaction_pde(x, u):
+            u_t = dde.grad.jacobian(u, x, i=0, j=1)
+            return u_t - rho * u + rho * u ** 2
+                       
+        def diffusion_pde(x, u):
+            u_t = dde.grad.jacobian(u, x, i=0, j=1)
+            u_xx = dde.grad.hessian(u, x, i=0, j=0)
+            return u_t - nu * u_xx
+             
+        def diffusion_reaction_pde(x, u):
+            u_t = dde.grad.jacobian(u, x, i=0, j=1)
+            u_xx = dde.grad.hessian(u, x, i=0, j=0)
+            return u_t - nu * u_xx - rho * u + rho * u ** 2
+
+        self.pde = convection_pde
+        self.set_pdeloss()
+
+        # refdata
+        self.create_ref_data(xgrid=DEFAULT_NUM_TEST_POINTS, nt=)
+
+        # BCs
+        def ic_func(x):
+            return np.sin(x[:,0:1])
+
+        self.add_bcs([{
+            'component': 0,
+            'function': ic_func,
+            'bc': (lambda _, on_initial: on_initial),
+            'type': 'ic'
+        }, {
+            'component': 0,
+            'function': (lambda _: -1), # as in Wu et al 2023
+            'bc': (lambda _, on_boundary: on_boundary),
+            'type': 'dirichlet'
+        }])
+
+        # train settings
+        self.training_points()  # default
+
+    def create_ref_data(self, nx=256, nt=100):
+        """
+        xgrid: number of X points
+        nt: number of time steps
+        """
+        h = 2 * np.pi / nx
+        x = np.arange(0, 2 * np.pi, h) # not inclusive of the last point
+        t = np.linspace(0, 1, nt).reshape(-1, 1)
+        X, T = np.meshgrid(x, t)
+
+        u0 = np.sin(x)
+        G = np.zeros_like(u0)
+
+        IKX_pos = 1j * np.arange(0, nx / 2 + 1, 1)
+        IKX_neg = 1j * np.arange(- nx / 2 + 1, 0, 1)
+        IKX = np.concatenate((IKX_pos, IKX_neg))
+        IKX2 = IKX * IKX
+
+        uhat0 = np.fft.fft(u0)
+        nu_factor = np.exp(- beta * IKX * T)
+        A = uhat0 - np.fft.fft(G) * 0 # at t=0, second term goes away
+        uhat = A * nu_factor + np.fft.fft(G) * T # for constant, fft(p) dt = fft(p)*T
+        u = np.real(np.fft.ifft(uhat))
+
+        u_vals = u.flatten() # why??
+        u_vals = u_vals.reshape(nt, nx).T # why?????
+
+        # adapt to how it is in baseclass
+        self.ref_data = np.hstack([x.reshape(-1, 1), u_vals])
