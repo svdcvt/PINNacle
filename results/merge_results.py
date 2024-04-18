774677ebcd73bb71a9a884bfd048406b8b1c0485
Sofya Dymchenko (bigfoot) <dymchens-ext@bigfoot7.u-ga.fr>
2024-04-18 11:21:08 +0200

    new plotting script for loss analysis, have to be cherry picked to main branch!

--- results/merge_results.py
+++ results/merge_results.py
@@ -72,9 +72,53 @@ def process_err(root_dir, err='l2res'):
         print(f'For {pde_name} values size: {result_kw_arrays[pde_name].shape}')
     return result_kw_arrays
 
+def process_loss(root_dir):
+    method_dirs = [subdir for subdir in os.listdir(root_dir) if substring in subdir]
+    print("Directories to process:\n", '\n '.join(method_dirs))
+
+    pde_id_name = []
+    for mdir in method_dirs:
+        # search for dir with biggest number processed pde (just in case)
+        csv_path = os.path.join(root_dir, mdir, 'result.csv')
+        if os.path.isfile(csv_path):
+            id_pde = pd.read_csv(csv_path, usecols=[0,1])
+            if len(id_pde) > len(pde_id_name):
+                pde_id_name = id_pde
+    P = len(pde_id_name)
+    M = len(method_dirs)
+
+    pde_ids = [f'{i}-0' for i in range(P)] # for now do not support mean+std
+    pde_names = pde_id_name['pde'].values
+    
+    result_kw_arrays = {
+            'methods': [x.split('-')[-1] for x in method_dirs]
+            }
+    for i, (pde_id, pde_name) in enumerate(zip(pde_ids, pde_names)):
+        # 20 : 7 x 20 array : num_pde X num_method X  num_log_iters -> processed further into 20 plots
+        pde_loss = []
+        for j, mdir in enumerate(method_dirs):
+            path = os.path.join(root_dir, mdir, pde_id, 'loss.txt') 
+            if os.path.isfile(path):
+                arr = np.loadtxt(path)
+                numloss = (arr.shape[1] - 1) // 3
+                weights = arr[:, 1+2*numloss:1+3*numloss]
+                train_loss_sum = np.sum(arr[:, 1:1+numloss]/weights, axis=1)
+                test_loss_sum = np.sum(arr[:, 1+numloss:1+2*numloss]/weights, axis=1)
+                pde_loss.append(train_loss_sum)
+                pde_loss.append(test_loss_sum)
+            else:
+                pde_loss.append(np.full(1, None))
+        l = max(len(x) for x in pde_loss)
+        pde_loss = [x if len(x) == l else np.append(x, np.full(l-len(x), None)) for x in pde_loss]
+        result_kw_arrays[pde_name] = np.stack(pde_loss, axis=0).reshape(M, 2, -1) # 7 x 2 x 20
+        print(f'For {pde_name} values size: {result_kw_arrays[pde_name].shape}')
+    return result_kw_arrays
 
 result_df = process_csv(root_directory)
 result_df.to_csv(os.path.join(tagpath, f'grouped_results-{tag}.csv'))
 
 result_error_kw_arrays = process_err(root_directory)
 np.savez(os.path.join(tagpath, f'grouped_errors-{tag}.npz'), **result_error_kw_arrays)
+
+result_loss_kw_arrays = process_loss(root_directory)
+np.savez(os.path.join(tagpath, f'grouped_loss-{tag}.npz'), **result_loss_kw_arrays)
--- results/results_plots_upd.py
+++ results/results_plots_upd.py
@@ -6,6 +6,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('tag')
+parser.add_argument('--range', nargs=3, default=[0, -1, 1], type=int)
 args = parser.parse_args()
 
 name = f'{args.tag}/grouped_results-{args.tag}'
@@ -50,10 +51,36 @@ for pp in range(0, P, RN):
         axes[i].set_title(pde)
         res_pde = results_errors[pde]
         for j in range(len(res_pde)):
-            axes[i].plot(res_pde[j], label=methods_order[j])
+            end = len(res_pde[j]) if args.range[1] == -1 else args.range[1]
+            axes[i].plot(res_pde[j][slice(*args.range)], label=methods_order[j])
+            axes[i].set_xticks(np.arange(args.range[0], end + 1, (end-args.range[0]) // 10, dtype=int))
         axes[i].set_yscale('log')
         axes[0].set_ylabel(error)
     axes[i].legend()
     pdf.savefig(fig)
 pdf.close()
 
+name = f'{args.tag}/grouped_loss-{args.tag}'
+results_loss = np.load(name + '.npz', allow_pickle=True)
+methods_order = results_loss['methods']
+pdes = list(results_loss.keys())[1:]
+P = len(pdes)
+
+# line plots of l2rel across iterations for each pde
+pdf = matplotlib.backends.backend_pdf.PdfPages(name + '.pdf')
+RN = 4 # maximum number of plots in a row
+for pp in range(0, P, RN):
+    fig, axes = plt.subplots(1, RN, figsize=(5 * RN + RN, 5))
+    for i, pde in enumerate(pdes[pp: pp + RN]):
+        axes[i].set_title(pde)
+        res_pde = results_loss[pde]
+        for j in range(len(res_pde)):
+            end = len(res_pde[j][0]) if args.range[1] == -1 else args.range[1]
+            p=axes[i].plot(res_pde[j][0][slice(*args.range)], label=methods_order[j])
+            axes[i].plot(res_pde[j][1][slice(*args.range)], linestyle='--', c=p[0].get_color())
+            axes[i].set_xticks(np.arange(args.range[0], end + 1, (end-args.range[0]) // 10, dtype=int))
+        axes[i].set_yscale('log')
+        axes[0].set_ylabel(error)
+    axes[i].legend()
+    pdf.savefig(fig)
+pdf.close()
